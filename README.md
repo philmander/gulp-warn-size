3d9b02492b07a5573766b0a6cd20ea0183b2a882
Phil Mander <philip.mander@gmail.com>
2016-03-30 14:17:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# gulp-warn-size
+
+> Warns if a file in a stream exceeds a given limit
+
+
+## Install
+
+```
+$ npm install --save-dev gulp-warn-size
+```
+
+
+## Usage
+
+```js
+const gulp = require('gulp');
+const size = require('gulp-warn-size');
+
+gulp.task('default', () =>
+	gulp.src('fixture.js')
+		.pipe(warn-size(2048)) //should not exceed 2kb
+		.on('error', () => process.exit(1))
+		.pipe(gulp.dest('dist'))
+);
+```
+
+`gulp-warn-size` does not display sizes. Use [gulp-size](https://github.com/sindresorhus/gulp-size) for this.
+
+
+## API
+
+### warn-size(limit)
+
+Type: `number`<br>
+Required
+
+The file size limit in bytes. Negative values will be ignored. Other values will cause the plugin to fail.
+
+### warn-size(options)
+
+#### options
+
+##### limit
+
+Type: `number`<br>
+Required
+
+Same as above
+
+##### errorOnFail
+
+Type: `boolean`<br>
+Default: `true`
+
+Emits an error from the stream if a file exceeds the given limit. Otherwise, just a warning will be logged to the console
+
+##### pretty
+
+Type: `boolean`<br>
+Default: true
+
+Displays prettified size: `1337 B` → `1.34 kB`.
+
+## License
+
+MIT. Phil Mander. 
+
+Adapted from gulp-size
---
+++ index.js
@@ -0,0 +1,79 @@
+'use strict';
+var gutil = require('gulp-util');
+var through = require('through2');
+var chalk = require('chalk');
+var prettyBytes = require('pretty-bytes');
+var StreamCounter = require('stream-counter');
+var objectAssign = require('object-assign');
+
+var PLUGIN_NAME = 'gulp-warn-size';
+
+var defaults = {
+    pretty: true,
+    errorOnFail: true
+};
+
+module.exports = function (opts) {
+    
+    if(typeof opts === 'object') {
+        opts = objectAssign(defaults, opts);
+    } else {
+        opts = objectAssign(defaults, {
+            limit: opts
+        });
+    }
+    
+    var limit = parseInt(opts.limit);
+    if(isNaN(limit)) {
+        throw new gutil.PluginError('gulp-warn-size', 'You must specify a (valid) file size limit in bytes');
+    }
+    
+    function log(what, size, limit) {
+        var fSize = opts.pretty ? prettyBytes(size) : (size + ' B');
+        var fLimit = opts.pretty ? prettyBytes(limit) : (limit + ' B');
+        var diff = Math.abs(limit - size);
+        var fDiff = opts.pretty ? prettyBytes(diff) : (diff + ' B');
+        
+        var message =
+            chalk.red('File size limit exceeded: ') + chalk.blue(what) + ' is ' + chalk.magenta(fSize) +
+            ' which exceeds its limit of ' + chalk.magenta(fLimit) +
+            ' by ' + chalk.red(fDiff) + ' ' + (opts.pretty ? chalk.magenta('(' +diff + ' B)') : '');
+        gutil.log(message);
+    }
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        var finish = function (err, size) {
+            if (err) {
+                cb(new gutil.PluginError(PLUGIN_NAME, err));
+                return;
+            }
+            
+            if(limit >= 0 && size > limit) {
+                log(file.relative, size, limit);
+
+                if(opts.errorOnFail) {
+                    cb(new gutil.PluginError(PLUGIN_NAME, 'Failing due to file limit being exceeded.'));
+                }
+            } else {
+                cb(null, file);
+            }
+        };
+
+        if (file.isStream()) {
+            file.contents.pipe(new StreamCounter())
+                .on('error', function(err) {
+                    finish(err);
+                })
+                .on('finish', function () {
+                    finish(null, this.bytes);
+                });
+        } else {
+            finish(null, file.contents.length);
+        }
+    });
+};
